9b982ea3cf0fb01ad3996a831db9f6f358c4e0a9
Louise <albertilouise@gmail.com>
2020-07-29 00:27:50 -0300

    adding files

---
+++ 01 - Introdução à Web Full Stack/Aula07-Javascript/js/scripts.js
@@ -0,0 +1,53 @@
+/* Exercício 1 */
+
+var intPositivo = 10;
+var intNegativo = -10;
+var decimal = 10.2;
+var word = 'hello';
+var varLogico = true;
+
+/* Exercício 2 */
+
+console.log(intPositivo,intNegativo,decimal,word,varLogico);
+
+/* Exercício 3 */
+
+var nome = 'Louise';
+var sobrenome = 'Alberti';
+console.log(nome + ' ' + sobrenome);
+
+/* Exercício 4 */
+/*
+var nome = 'Clara';
+console.log(Nome);
+            ^
+Ocorre um erro, pois a variável 'Nome' não foi definida
+*/
+
+/* Exercício 5 */
+
+var nome = 'Clara';
+console.log(nome + ' ' + sobrenome);
+
+/* Exercício 6 */
+
+console.log('Escrever um texto em mais de uma linha \ne imprimir usando a função console.log()');
+
+/* Exercício 7 */
+
+var valorNulo = null;
+var naoDefinido;
+
+console.log(valorNulo,naoDefinido);
+
+/* Exercício 8 */
+var arrayFrutas = ['pêssego','goiaba','morango','uva','amora'];
+
+for(fruta in arrayFrutas) {
+    console.log(arrayFrutas[fruta]);
+}
+
+
+/* Exercício 9 */
+
+var digitalHouse = 'Digital House, a escola de programação que forma novas gerações de coders e profissionais digitais para imaginar, inovar e criar o que sempre sonharam.';
